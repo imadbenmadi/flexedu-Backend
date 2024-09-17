7ac8030511a5624f29a609c68176c3a3f68b4d64
imadbenmadi <imadbenmadi891@gmail.com>
2024-09-17 15:42:45 +0200

    removing unesssary uploads

--- Routes/Uploads/Upload.js
+++ Routes/Uploads/Upload.js
@@ -6,9 +6,7 @@ const Upload_Student_ProfilePic = require("./ProfilePic/Student_ProfilePic");
 const Delete_Student_ProfilePic = require("./ProfilePic/Student_ProfilePic_Delete");
 const Upload_Payment_ScreenShot = require("./Payment_screenShots/Payment_screenShots");
 const Delete_Payment_ScreenShot = require("./Payment_screenShots/Payment_screenShots_Delete");
-const Upload_Student_PortfolioItem = require("./PortfolioItems/Portfolioitem");
-const Delete_Student_PortfolioItem = require("./PortfolioItems/Portfolioitem_Delete");
-const Upload_Work = require("./Work/Work");
+
 const Student_Middlware = require("../../Middlewares/Student");
 const Teacher_Middlware = require("../../Middlewares/Teacher");
 const cookieParser = require("cookie-parser");
@@ -70,33 +68,6 @@ router.delete(
     // },
     Teacher_Middlware,
     Delete_Payment_ScreenShot
-);
-router.post(
-    "/Work",
-    (req, res, next) => {
-        req.body = req.fields;
-        next();
-    },
-    Student_Middlware,
-    Upload_Work
-);
-router.post(
-    "/Student/PortfolioItem",
-    // (req, res, next) => {
-    //     req.body = req.fields;
-    //     next();
-    // },
-    Student_Middlware,
-    Upload_Student_PortfolioItem
-);
-router.delete(
-    "/Student/PortfolioItem",
-    // (req, res, next) => {
-    //     req.body = req.fields;
-    //     next();
-    // },
-    Student_Middlware,
-    Delete_Student_PortfolioItem
 );
 
 module.exports = router;
