29b5be4ef213a3f8db3da538d71cb14519d5041c
imadbenmadi <imadbenmadi891@gmail.com>
2024-09-24 01:27:01 +0200

    Refactor Get_Vedio API response to include capitalized key for video object

--- Controllers/Teacher/Courses.js
+++ Controllers/Teacher/Courses.js
@@ -76,7 +76,7 @@ const Get_Vedio = async (req, res) => {
         });
         if (!vedio) return res.status(404).json({ error: "vedio not found." });
 
-        return res.status(200).json({ vedio });
+        return res.status(200).json({ Vedio : vedio });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Internal server error." });
