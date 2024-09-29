62098f3c5654fcd537a3b1037862db0d79552f21
imadbenmadi <imadbenmadi891@gmail.com>
2024-09-30 00:25:39 +0200

    Refactor routes to use Summary instead of Course for summary purchase requests

--- Routes/Admin/Payment.js
+++ Routes/Admin/Payment.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Courses = require("../../Models/Course");
+const Summary = require("../../Models/Summary");
 const Course_Purcase_Requests = require("../../Models/Course_Purcase_Requests");
+const Summary_Purcase_Requests = require("../../Models/Summary_Purcase_Requests");
 const Course_Progress = require("../../Models/Course_Progress");
 const Students = require("../../Models/Student");
 const Teachers = require("../../Models/Teacher");
@@ -12,7 +14,7 @@ const {
     Student_Notifications,
     Teacher_Notifications,
 } = require("../../Models/Notifications");
-router.get("/", Admin_midllware, async (req, res) => {
+router.get("/Courses", Admin_midllware, async (req, res) => {
     try {
         const courses_Purcase_Requests = await Course_Purcase_Requests.findAll({
             // where: { status: "Pending" },
@@ -32,7 +34,7 @@ router.get("/", Admin_midllware, async (req, res) => {
         res.status(500).json({ message: err });
     }
 });
-router.get("/:courseId", Admin_midllware, async (req, res) => {
+router.get("/Courses/:courseId", Admin_midllware, async (req, res) => {
     const courseId = req.params.courseId;
     if (!courseId)
         return res
@@ -56,45 +58,7 @@ router.get("/:courseId", Admin_midllware, async (req, res) => {
         res.status(500).json({ message: err });
     }
 });
-router.get("/Accepted", Admin_midllware, async (req, res) => {
-    try {
-        const courses = await Courses.findAll({
-            // where: { status: "Pending" },
-            where: {
-                isPayment_ScreenShot_uploaded: true,
-                status: "Payed",
-                isPayment_ScreenShot_Rejected: false,
-            },
-            order: [["createdAt", "DESC"]],
-        });
-        res.status(200).json({ courses: courses });
-    } catch (err) {
-        console.error("Error fetching Course courses:", err);
-        res.status(500).json({ message: err });
-    }
-});
-
-// router.get("/:courseId", Admin_midllware, async (req, res) => {
-//     const courseId = req.params.courseId;
-//     if (!courseId)
-//         return res
-//             .status(409)
-//             .json({ message: "Missing data CourseId is required" });
-//     try {
-//         const courses = await Courses.findOne({
-//             where: {
-//                 // status: "Pending",
-//                 CourseId: courseId,
-//             },
-//             order: [["createdAt", "DESC"]],
-//         });
-//         res.status(200).json({ courses: courses });
-//     } catch (err) {
-//         console.error("Error fetching Course courses:", err);
-//         res.status(500).json({ message: err });
-//     }
-// });
-router.post("/:courseId/Accept", Admin_midllware, async (req, res) => {
+router.post("/Courses/:courseId/Accept", Admin_midllware, async (req, res) => {
     const { courseId } = req.params;
     const { studentId } = req.body;
 
@@ -172,7 +136,7 @@ router.post("/:courseId/Accept", Admin_midllware, async (req, res) => {
         });
     }
 });
-router.post("/:courseId/Reject", Admin_midllware, async (req, res) => {
+router.post("/Courses/:courseId/Reject", Admin_midllware, async (req, res) => {
     const { courseId } = req.params;
     const { studentId } = req.body; // We need studentId from the request body, same as in Accept
 
@@ -248,6 +212,212 @@ router.post("/:courseId/Reject", Admin_midllware, async (req, res) => {
         // Rollback the transaction in case of an error
         await t.rollback();
         console.error("Error processing course payment rejection:", err);
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: err.message,
+        });
+    }
+});
+
+router.get("/Summary", Admin_midllware, async (req, res) => {
+    try {
+        const summary_Purcase_Requests = await Summary_Purcase_Requests.findAll(
+            {
+                // where: { status: "Pending" },
+                // where: { isPayment_ScreenShot_uploaded: true },
+                where: {
+                    StudentId: { [Op.not]: null },
+                    status: "pending",
+                },
+                include: [{ model: Students }, { model: Summary }],
+                order: [["createdAt", "DESC"]],
+            }
+        );
+        res.status(200).json({
+            summary_Purcase_Requests: summary_Purcase_Requests,
+        });
+    } catch (err) {
+        console.error("Error fetching Summary courses:", err);
+        res.status(500).json({ message: err });
+    }
+});
+router.get("/Summary/:summaryId", Admin_midllware, async (req, res) => {
+    const summaryId = req.params.summaryId;
+    if (!summaryId)
+        return res.status(409).json({
+            message: "Missing data SummaryId is required",
+        });
+
+    try {
+        const summary_Purcase_Requests = await Summary_Purcase_Requests.findOne(
+            {
+                where: {
+                    id: summaryId,
+                    StudentId: { [Op.not]: null },
+                },
+                include: [{ model: Students }, { model: Summary }],
+                order: [["createdAt", "DESC"]],
+            }
+        );
+        res.status(200).json({
+            summary_Purcase_Requests: summary_Purcase_Requests,
+        });
+    } catch (err) {
+        console.error("Error fetching Summary courses:", err);
+        res.status(500).json({ message: err });
+    }
+});
+
+router.post("/Summary/:summaryId/Accept", Admin_midllware, async (req, res) => {
+    const { summaryId } = req.params;
+    const { studentId } = req.body;
+
+    if (!summaryId || !studentId) {
+        return res.status(409).json({
+            message: "Missing data: SummaryId and StudentId are required",
+        });
+    }
+
+    const t = await sequelize.transaction(); // Start a transaction
+
+    try {
+        const summary = await Summary.findOne({ where: { id: summaryId } });
+        const payment_request = await Summary_Purcase_Requests.findOne({
+            where: { SummaryId: summaryId, StudentId: studentId },
+        });
+        if (!summary || !payment_request) {
+            await t.rollback();
+            return res
+                .status(404)
+                .json({ message: "Summary or payment request not found" });
+        }
+        await payment_request.update(
+            { status: "accepted" },
+            { transaction: t }
+        );
+        await Course_Progress.create(
+            {
+                StudentId: studentId,
+                SummaryId: summaryId,
+                // Course_Videos_number: course.Vedios_count,
+            },
+            { transaction: t }
+        );
+        try {
+            await Teacher_Notifications.create(
+                {
+                    title: "Payment received",
+                    text: "A new student has paid the fees for your Summary.",
+                    type: "payment_received",
+                    TeacherId: summary.TeacherId,
+                    link: `/Teacher/Payment/Summary/${summary.id}`,
+                },
+                { transaction: t }
+            );
+            await Student_Notifications.create(
+                {
+                    title: "Your payment has been accepted",
+                    text: "You can now access the Summary content.",
+                    type: "payment_accepted",
+                    StudentId: studentId,
+                    link: `/Student/Purchased/Summary/${summary.id}`,
+                },
+                { transaction: t }
+            );
+        } catch (error) {
+            await t.rollback();
+            return res
+                .status(500)
+                .json({ message: "Notification Error", error: error.message });
+        }
+        await t.commit(); // Commit transaction if all went well
+        res.status(200).json({ message: "Summary payment accepted" });
+    } catch (err) {
+        await t.rollback(); // Rollback transaction on error
+        console.error("Error processing Summary payment approval:", err);
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: err.message,
+        });
+    }
+});
+router.post("/Summary/:summaryId/Reject", Admin_midllware, async (req, res) => {
+    const { summaryId } = req.params;
+    const { studentId } = req.body; // We need studentId from the request body, same as in Accept
+
+    // Check if courseId and studentId are provided
+    if (!summaryId || !studentId) {
+        return res.status(409).json({
+            message: "Missing data: SummaryId and StudentId are required",
+        });
+    }
+
+    // Start a transaction
+    const t = await sequelize.transaction();
+
+    try {
+        // Find the course and payment request
+        const summary = await Summary.findOne({
+            where: { id: summaryId },
+            transaction: t,
+        });
+        const payment_request = await Summary_Purcase_Requests.findOne({
+            where: { SummaryId: summaryId, StudentId: studentId },
+            transaction: t,
+        });
+
+        // Check if the course and payment request exist
+        if (!summary || !payment_request) {
+            await t.rollback(); // Rollback transaction if not found
+            return res
+                .status(404)
+                .json({ message: "Summary or payment request not found" });
+        }
+
+        // Update the payment request status to rejected
+        await payment_request.update(
+            { status: "rejected" },
+            { transaction: t }
+        );
+
+        // Remove the course progress for the student if it exists
+        // await Course_Progress.destroy({
+        //     where: { CourseId: courseId, StudentId: studentId },
+        //     transaction: t,
+        // });
+
+        // Send notifications to both teacher and student
+
+        try {
+            // Teacher notification
+
+            // Student notification
+            await Student_Notifications.create(
+                {
+                    title: "Your payment has been rejected",
+                    text: `Your payment for the Summary "${summary.Title}" has been rejected. Please contact support for more details.`,
+                    type: "payment_rejected",
+                    StudentId: studentId,
+                    link: `/Student/Summary/${summary.id}/Enrollment/?rejected=true`,
+                },
+                { transaction: t }
+            );
+        } catch (error) {
+            await t.rollback(); // Rollback transaction if notification fails
+            return res.status(500).json({ error: error.message });
+        }
+
+        // Commit the transaction
+        await t.commit();
+
+        // Respond with success
+        res.status(200).json({
+            message: "Summary payment rejected successfully",
+        });
+    } catch (err) {
+        // Rollback the transaction in case of an error
+        await t.rollback();
+        console.error("Error processing Summary payment rejection:", err);
         res.status(500).json({
             message: "Internal Server Error",
             error: err.message,
