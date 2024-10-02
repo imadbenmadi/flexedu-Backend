7d10f6c5457d7d6b5c4c6071814292f0da15dc1d
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-10-03 00:31:52 +0100

    Refactor Student and Teacher routes for improved readability and add Search_Summaries functionality

--- Controllers/Student/Summaries.js
+++ Controllers/Student/Summaries.js
@@ -3,83 +3,116 @@ const Students = require("../../Models/Student");
 const Summary_Purcase_Requests = require("../../Models/Summary_Purcase_Requests");
 const Review_Summary = require("../../Models/Review_Summary");
 const Get_Summaries = async (req, res) => {
-    const userId = req.decoded.userId;
-    if (!userId)
-        return res.status(401).json({ error: "Unauthorized , missing userId" });
-    try {
-        const summarys = await Summary.findAll({
-            order: [["createdAt", "DESC"]],
-        });
-        if (!summarys)
-            return res.status(404).json({ error: "No summarys found." });
-        return res.status(200).json({ Summaries: summarys });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+  const userId = req.decoded.userId;
+  if (!userId)
+    return res.status(401).json({ error: "Unauthorized , missing userId" });
+  try {
+    const summarys = await Summary.findAll({
+      order: [["createdAt", "DESC"]],
+    });
+    if (!summarys) return res.status(404).json({ error: "No summarys found." });
+    return res.status(200).json({ Summaries: summarys });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
+// const Get_Summaries = async (req, res) => {
+//   const userId = req.decoded.userId;
+//   if (!userId)
+//     return res.status(401).json({ error: "Unauthorized , missing userId" });
+//   try {
+//     const summarys = await Summary.findAll({
+//       order: [["createdAt", "DESC"]],
+//     });
+//     if (!summarys) return res.status(404).json({ error: "No summarys found." });
+//     return res.status(200).json({ Summaries: summarys });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ error: "Internal server error." });
+//   }
+// };
 
 const GetSummary = async (req, res) => {
-    const userId = req.decoded.userId;
-    const summaryId = req.params.summaryId;
-    if (!userId || !summaryId)
-        return res
-            .status(409)
-            .json({ error: "Unauthorized , missing userId or summaryId" });
-    try {
-        const summary = await Summary.findOne({
-            where: {
-                id: summaryId,
-                // TeacherId: userId,
-            },
+  const userId = req.decoded.userId;
+  const summaryId = req.params.summaryId;
+  if (!userId || !summaryId)
+    return res
+      .status(409)
+      .json({ error: "Unauthorized , missing userId or summaryId" });
+  try {
+    const summary = await Summary.findOne({
+      where: {
+        id: summaryId,
+        // TeacherId: userId,
+      },
 
-            order: [["createdAt", "DESC"]],
-        });
-        if (!summary)
-            return res.status(404).json({ error: "summary not found." });
-        let paymentStatus = null;
-        const student = await Students.findOne({
-            where: {
-                id: userId,
-            },
-        });
-        const purcase = await Summary_Purcase_Requests.findOne({
-            where: {
-                id: summaryId,
-                StudentId: userId,
-            },
-        });
-        if (purcase) {
-            paymentStatus = purcase.status;
-        }
-
-        if (!student)
-            return res
-                .status(409)
-                .json({ error: "Unauthorized , not a student" });
-        let isReviewed = false;
-        const reviews = await Review_Summary.findAll({
-            where: {
-                StudentId: userId,
-            },
-        });
-        if (reviews) {
-            isReviewed = true;
-        }
-
-        return res.status(200).json({
-            paymentStatus,
-            purcase,
-            Summary: summary,
-            isReviewed,
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
+      order: [["createdAt", "DESC"]],
+    });
+    if (!summary) return res.status(404).json({ error: "summary not found." });
+    let paymentStatus = null;
+    const student = await Students.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    const purcase = await Summary_Purcase_Requests.findOne({
+      where: {
+        id: summaryId,
+        StudentId: userId,
+      },
+    });
+    if (purcase) {
+      paymentStatus = purcase.status;
     }
+
+    if (!student)
+      return res.status(409).json({ error: "Unauthorized , not a student" });
+    let isReviewed = false;
+    const reviews = await Review_Summary.findAll({
+      where: {
+        StudentId: userId,
+      },
+    });
+    if (reviews) {
+      isReviewed = true;
+    }
+
+    return res.status(200).json({
+      paymentStatus,
+      purcase,
+      Summary: summary,
+      isReviewed,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 
+const Search_Summaries = async (req, res) => {
+  const userId = req.decoded.userId;
+  const { search } = req.query;
+  if (!userId)
+    return res.status(401).json({ error: "Unauthorized , missing userId" });
+  try {
+    const summarys = await Summary.findAll({
+      where: {
+        Title: {
+          [Op.like]: `%${search}%`,
+        },
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    if (!summarys) return res.status(404).json({ error: "No summarys found." });
+    return res.status(200).json({ Summaries: summarys });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+};
 module.exports = {
-    GetSummary,
-    Get_Summaries,
+  GetSummary,
+  Get_Summaries,
+  Search_Summaries,
 };
--- Controllers/Teacher/Summaries.js
+++ Controllers/Teacher/Summaries.js
@@ -5,156 +5,150 @@ const Summaries = require("../../Models/Summary");
 const Students = require("../../Models/Student");
 
 const GetSummaries = async (req, res) => {
-    const userId = req.decoded.userId;
-    if (!userId)
-        return res.status(401).json({ error: "Unauthorized , missing userId" });
-    try {
-        const summarys = await Summaries.findAll({
-            where: {
-                TeacherId: userId,
-            },
-            order: [["createdAt", "DESC"]],
-        });
-        if (!summarys)
-            return res.status(404).json({ error: "No summarys found." });
-        return res.status(200).json({ Summaries: summarys });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+  const userId = req.decoded.userId;
+  if (!userId)
+    return res.status(401).json({ error: "Unauthorized , missing userId" });
+  try {
+    const summarys = await Summaries.findAll({
+      where: {
+        TeacherId: userId,
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    if (!summarys) return res.status(404).json({ error: "No summarys found." });
+    return res.status(200).json({ Summaries: summarys });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 const GetSummary = async (req, res) => {
-    const userId = req.decoded.userId;
-    const summaryId = req.params.summaryId;
-    if (!userId || !summaryId) {
-        return res
-            .status(409)
-            .json({ error: "Unauthorized, missing userId or summaryId" });
+  const userId = req.decoded.userId;
+  const summaryId = req.params.summaryId;
+  if (!userId || !summaryId) {
+    return res
+      .status(409)
+      .json({ error: "Unauthorized, missing userId or summaryId" });
+  }
+  try {
+    const summary = await Summaries.findOne({
+      where: {
+        id: summaryId,
+        TeacherId: userId,
+      },
+    });
+    if (!summary) {
+      return res.status(404).json({ error: "Summary not found." });
     }
-    try {
-        const summary = await Summaries.findOne({
-            where: {
-                id: summaryId,
-                TeacherId: userId,
-            },
-        });
-        if (!summary) {
-            return res.status(404).json({ error: "Summary not found." });
-        }
-        return res.status(200).json({ Summary: summary });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+    return res.status(200).json({ Summary: summary });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 
 const DeleteSummary = async (req, res) => {
-    const userId = req.decoded.userId;
-    const summaryId = req.params.summaryId;
-    if (!userId || !summaryId) {
-        return res
-            .status(409)
-            .json({ error: "Unauthorized, missing userId or summaryId" });
+  const userId = req.decoded.userId;
+  const summaryId = req.params.summaryId;
+  if (!userId || !summaryId) {
+    return res
+      .status(409)
+      .json({ error: "Unauthorized, missing userId or summaryId" });
+  }
+  try {
+    const summary = await Summaries.findOne({
+      where: {
+        id: summaryId,
+        TeacherId: userId,
+      },
+    });
+    if (!summary) {
+      return res.status(404).json({ error: "Summary not found." });
     }
-    try {
-        const summary = await Summaries.findOne({
-            where: {
-                id: summaryId,
-                TeacherId: userId,
-            },
-        });
-        if (!summary) {
-            return res.status(404).json({ error: "Summary not found." });
-        }
-        await summary.destroy();
-        // Additional logic to delete related data like videos, student ownership, etc.
-        return res.status(200).json({ message: "Summary deleted." });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+    await summary.destroy();
+    // Additional logic to delete related data like videos, student ownership, etc.
+    return res.status(200).json({ message: "Summary deleted." });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 
 const EditSummary = async (req, res) => {
-    const userId = req.decoded.userId;
-    const summaryId = req.params.summaryId;
-    const { Title, Description, Price, Category } = req.body;
+  const userId = req.decoded.userId;
+  const summaryId = req.params.summaryId;
+  const { Title, Description, Price, Category } = req.body;
 
-    if (
-        !userId ||
-        !summaryId ||
-        !Title ||
-        !Description ||
-        !Price ||
-        !Category
-    ) {
-        return res.status(409).json({
-            error: "Unauthorized, missing userId or summaryId or Title or Description or Price or Category",
-        });
+  if (!userId || !summaryId || !Title || !Description || !Price || !Category) {
+    return res.status(409).json({
+      error:
+        "Unauthorized, missing userId or summaryId or Title or Description or Price or Category",
+    });
+  }
+
+  try {
+    const summary = await Summaries.findOne({
+      where: {
+        id: summaryId,
+        TeacherId: userId,
+      },
+    });
+    if (!summary) {
+      return res.status(404).json({ error: "Summary not found." });
     }
 
-    try {
-        const summary = await Summaries.findOne({
-            where: {
-                id: summaryId,
-                TeacherId: userId,
-            },
-        });
-        if (!summary) {
-            return res.status(404).json({ error: "Summary not found." });
-        }
+    summary.Title = Title;
+    summary.Description = Description;
+    summary.Price = Price;
+    summary.Category = Category;
+    await summary.save();
 
-        summary.Title = Title;
-        summary.Description = Description;
-        summary.Price = Price;
-        summary.Category = Category;
-        await summary.save();
-
-        return res.status(200).json({ message: "Summary updated." });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+    return res.status(200).json({ message: "Summary updated." });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 const add_Summary = async (req, res) => {
-    if (req.decoded.userType !== "teacher") {
-        return res
-            .status(401)
-            .json({ error: "Forbidden: only teachers can create summarys." });
-    }
-    const userId = req.decoded.userId;
-    const { Title, Description, Price, Category } = req.body;
+  if (req.decoded.userType !== "teacher") {
+    return res
+      .status(401)
+      .json({ error: "Forbidden: only teachers can create summarys." });
+  }
+  const userId = req.decoded.userId;
+  const { Title, Description, Price, Category } = req.body;
 
-    // Check if all required fields are present
-    if (!userId || !Title || !Description || !Price || !Category) {
-        return res.status(409).json({
-            error: "Unauthorized: missing userId, Title, Description, Price, duration, or Category",
-        });
-    }
+  // Check if all required fields are present
+  if (!userId || !Title || !Description || !Price || !Category) {
+    return res.status(409).json({
+      error:
+        "Unauthorized: missing userId, Title, Description, Price, duration, or Category",
+    });
+  }
 
-    try {
-        // Create a new summary
-        const summary = await Summaries.create({
-            Title: Title, // Match case with your model
-            Description: Description, // Match case with your model
-            Price: Price, // Match case with your model
-            Category: Category, // Category is mandatory per your model
-            TeacherId: userId, // Set the teacher (user) ID
-        });
+  try {
+    // Create a new summary
+    const summary = await Summaries.create({
+      Title: Title, // Match case with your model
+      Description: Description, // Match case with your model
+      Price: Price, // Match case with your model
+      Category: Category, // Category is mandatory per your model
+      TeacherId: userId, // Set the teacher (user) ID
+    });
 
-        // Send success response
-        return res
-            .status(200)
-            .json({ message: "Course created successfully.", summary });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error." });
-    }
+    // Send success response
+    return res
+      .status(200)
+      .json({ message: "Course created successfully.", summary });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 };
 module.exports = {
-    GetSummaries,
-    DeleteSummary,
-    GetSummary,
-    EditSummary,
-    add_Summary,
+  GetSummaries,
+  DeleteSummary,
+  GetSummary,
+  EditSummary,
+  add_Summary,
 };
