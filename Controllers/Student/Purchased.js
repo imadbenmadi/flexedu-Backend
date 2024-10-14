e24c3bd24eb914a7a8188b065a50b96c14ba040e
imadbenmadi <imadbenmadi891@gmail.com>
2024-10-15 00:03:10 +0200

    Refactor course retrieval and progress update logic with improved error handling and video inclusion

--- Controllers/Student/Purchased.js
+++ Controllers/Student/Purchased.js
@@ -9,196 +9,205 @@ const Summary_Purcase_Requests = require("../../Models/Summary_Purcase_Requests"
 const Reviews = require("../../Models/Review_Course");
 const Review_Summary = require("../../Models/Review_Summary");
 const GetPurchased = async (req, res) => {
-  const userId = req.decoded.userId;
-  if (!userId)
-    return res.status(401).json({ error: "Unauthorized , missing userId" });
-  try {
-    let course_Purcase_Requests = [];
-    course_Purcase_Requests = await Course_Purcase_Requests.findAll({
-      where: {
-        StudentId: userId,
-        status: "accepted",
-      },
-      include: [
-        {
-          model: Courses,
-          // as: "Course_Video",
-          include: [
-            {
-              model: Course_Video,
-              // as: "Course_Video",
+    const userId = req.decoded.userId;
+    if (!userId)
+        return res.status(401).json({ error: "Unauthorized , missing userId" });
+    try {
+        let course_Purcase_Requests = [];
+        course_Purcase_Requests = await Course_Purcase_Requests.findAll({
+            where: {
+                StudentId: userId,
+                status: "accepted",
             },
-          ],
-        },
-      ],
+            include: [
+                {
+                    model: Courses,
+                    // as: "Course_Video",
+                    include: [
+                        {
+                            model: Course_Video,
+                            // as: "Course_Video",
+                        },
+                    ],
+                },
+            ],
 
-      order: [["createdAt", "DESC"]],
-    });
-    let summary_Purcase_Requests = [];
-    summary_Purcase_Requests = await Summary_Purcase_Requests.findAll({
-      where: {
-        StudentId: userId,
-        status: "accepted",
-      },
-      include: [
-        {
-          model: Summary,
-          // as: "Course_Video",
-        },
-      ],
-      order: [["createdAt", "DESC"]],
-    });
+            order: [["createdAt", "DESC"]],
+        });
+        let summary_Purcase_Requests = [];
+        summary_Purcase_Requests = await Summary_Purcase_Requests.findAll({
+            where: {
+                StudentId: userId,
+                status: "accepted",
+            },
+            include: [
+                {
+                    model: Summary,
+                    // as: "Course_Video",
+                },
+            ],
+            order: [["createdAt", "DESC"]],
+        });
 
-    return res.status(200).json({
-      course_Purcase_Requests: course_Purcase_Requests,
-      summary_Purcase_Requests: summary_Purcase_Requests,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error." });
-  }
+        return res.status(200).json({
+            course_Purcase_Requests: course_Purcase_Requests,
+            summary_Purcase_Requests: summary_Purcase_Requests,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error." });
+    }
 };
 
 const GetPurchasedCourse = async (req, res) => {
-  const userId = req.decoded.userId;
-  const courseId = req.params.courseId;
-  if (!userId || !courseId)
-    return res
-      .status(409)
-      .json({ error: "Unauthorized , missing userId or courseId" });
-  try {
-    const course = await Courses.findOne({
-      where: {
-        id: courseId,
-        // TeacherId: userId,
-      },
-      include: [
-        {
-          model: Course_Video,
-          // as: "Course_Video",
-        },
-      ],
-      order: [["createdAt", "DESC"]],
-    });
-    if (!course) return res.status(404).json({ error: "course not found." });
-    let isEnrolled = false;
-    let paymentStatus = null;
-    let isReviewed = false;
-    const student = await Students.findOne({
-      where: {
-        id: userId,
-      },
-    });
-    const purcase = await Course_Purcase_Requests.findOne({
-      where: {
-        CourseId: courseId,
-        StudentId: userId,
-      },
-      include: [
-        {
-          model: Courses,
-          // as: "Course_Video",
-        },
-      ],
-    });
-    if (purcase) {
-      paymentStatus = purcase.status;
-    }
+    const userId = req.decoded.userId;
+    const courseId = req.params.courseId;
+    if (!userId || !courseId)
+        return res
+            .status(409)
+            .json({ error: "Unauthorized , missing userId or courseId" });
+    try {
+        const course = await Courses.findOne({
+            where: {
+                id: courseId,
+                // TeacherId: userId,
+            },
+            include: [
+                {
+                    model: Course_Video,
+                    // as: "Course_Video",
+                },
+            ],
+            order: [["createdAt", "DESC"]],
+        });
+        if (!course)
+            return res.status(404).json({ error: "course not found." });
+        let isEnrolled = false;
+        let paymentStatus = null;
+        let isReviewed = false;
+        const student = await Students.findOne({
+            where: {
+                id: userId,
+            },
+        });
+        const purcase = await Course_Purcase_Requests.findOne({
+            where: {
+                CourseId: courseId,
+                StudentId: userId,
+            },
+            include: [
+                {
+                    model: Courses,
+                    // as: "Course_Video",
+                },
+            ],
+        });
+        if (purcase) {
+            paymentStatus = purcase.status;
+        }
 
-    if (!student)
-      return res.status(409).json({ error: "Unauthorized , not a student" });
-    const course_progress = await Course_Progress.findOne({
-      where: {
-        StudentId: userId,
-        CourseId: courseId,
-      },
-    });
-    if (course_progress) {
-      isEnrolled = true;
+        if (!student)
+            return res
+                .status(409)
+                .json({ error: "Unauthorized , not a student" });
+        const course_progress = await Course_Progress.findOne({
+            where: {
+                StudentId: userId,
+                CourseId: courseId,
+            },
+        });
+        if (course_progress) {
+            isEnrolled = true;
+        }
+        const reviews = await Reviews.findOne({
+            where: {
+                StudentId: userId,
+                CourseId: courseId,
+            },
+        });
+        if (reviews) {
+            isReviewed = true;
+        }
+        return res.status(200).json({
+            isEnrolled: isEnrolled,
+            isReviewed: isReviewed,
+            paymentStatus,
+            purcase,
+            Course: course,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error." });
     }
-    const reviews = await Reviews.findAll({
-      where: {
-        StudentId: userId,
-      },
-    });
-    if (reviews) {
-      isReviewed = true;
-    }
-    return res.status(200).json({
-      isEnrolled: isEnrolled,
-      isReviewed: isReviewed,
-      paymentStatus,
-      purcase,
-      Course: course,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error." });
-  }
 };
 const GetPurchasedSummary = async (req, res) => {
-  const userId = req.decoded.userId;
-  const summaryId = req.params.summaryId;
-  if (!userId || !summaryId)
-    return res
-      .status(409)
-      .json({ error: "Unauthorized , missing userId or summaryId" });
-  try {
-    const summary = await Summary.findOne({
-      where: {
-        id: summaryId,
-        // TeacherId: userId,
-      },
+    const userId = req.decoded.userId;
+    const summaryId = req.params.summaryId;
+    if (!userId || !summaryId)
+        return res
+            .status(409)
+            .json({ error: "Unauthorized , missing userId or summaryId" });
+    try {
+        const summary = await Summary.findOne({
+            where: {
+                id: summaryId,
+                // TeacherId: userId,
+            },
 
-      order: [["createdAt", "DESC"]],
-    });
-    if (!summary) return res.status(404).json({ error: "summary not found." });
-    let paymentStatus = null;
-    const student = await Students.findOne({
-      where: {
-        id: userId,
-      },
-    });
-    if (!student)
-      return res.status(409).json({ error: "Unauthorized , not a student" });
-    const purcase = await Summary_Purcase_Requests.findOne({
-      where: {
-        SummaryId: summaryId,
-        StudentId: userId,
-      },
-      include: [
-        {
-          model: Summary,
-          // as: "Course_Video",
-        },
-      ],
-    });
-    if (purcase) {
-      paymentStatus = purcase.status;
-    }
-    const reviews = await Review_Summary.findAll({
-      where: {
-        StudentId: userId,
-      },
-    });
-    if (reviews) {
-      isReviewed = true;
-    }
+            order: [["createdAt", "DESC"]],
+        });
+        if (!summary)
+            return res.status(404).json({ error: "summary not found." });
+        let paymentStatus = null;
+        const student = await Students.findOne({
+            where: {
+                id: userId,
+            },
+        });
+        if (!student)
+            return res
+                .status(409)
+                .json({ error: "Unauthorized , not a student" });
+        const purcase = await Summary_Purcase_Requests.findOne({
+            where: {
+                SummaryId: summaryId,
+                StudentId: userId,
+            },
+            include: [
+                {
+                    model: Summary,
+                    // as: "Course_Video",
+                },
+            ],
+        });
+        if (purcase) {
+            paymentStatus = purcase.status;
+        }
+        const reviews = await Review_Summary.findOne({
+            where: {
+                StudentId: userId,
+                SummaryId: summaryId,
+            },
+        });
+        let isReviewed = false;
+        if (reviews) {
+            isReviewed = true;
+        }
 
-    return res.status(200).json({
-      paymentStatus,
-      purcase,
-      Summary: summary,
-      isReviewed,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error." });
-  }
+        return res.status(200).json({
+            paymentStatus,
+            purcase,
+            Summary: summary,
+            isReviewed,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error." });
+    }
 };
 
 module.exports = {
-  GetPurchasedCourse,
-  GetPurchased,
-  GetPurchasedSummary,
+    GetPurchasedCourse,
+    GetPurchased,
+    GetPurchasedSummary,
 };
